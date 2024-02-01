c6500b30c6fbc247ded28fb2cac3ca71fc2578e6
Triandafillos <georgepapadakis1111@gmail.com>
2024-01-31 19:54:21 -0700

    c

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 let button1 = document.querySelector("#healthButton");
+let healthText = document.querySelector("#healthText");
+let health = 100;
+
+button1.onclick = ouch;
+
+function ouch() {
+    health = health - 10;
+    healthText = health;
+}
