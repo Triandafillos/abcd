b281bfa7872bdaae6e36be08a48fbf65da3a2848
Triandafillos <georgepapadakis1111@gmail.com>
2024-02-01 20:20:24 -0700

    c

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let button1 = document.querySelector("#healthButton");
+let loginButton = document.querySelector("#login");
+let loginSection = document.querySelector("#loginSection");
 let healthText = document.querySelector("#healthText");
 let health = 100;
 
 button1.onclick = ouch;
+loginButton.onclick = login;
 
 function ouch() {
     if(health >0){
@@ -12,4 +15,8 @@ function ouch() {
     if (health < 50) {
         healthText.style.color = "red";
     }
+}
+
+function login(){
+    loginSection.display = none;
 }
